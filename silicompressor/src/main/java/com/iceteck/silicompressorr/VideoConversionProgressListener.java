d45397923b8c22e82ef5a6c04d7cd53c6e179abb
PGMacDesign <PGMacDesign@gmail.com>
2019-07-09 13:47:59 -0700

    Updating the callback to include an estimated time of completion. Added in the ability to cancel active compression jobs on videos

--- silicompressor/src/main/java/com/iceteck/silicompressorr/VideoConversionProgressListener.java
+++ silicompressor/src/main/java/com/iceteck/silicompressorr/VideoConversionProgressListener.java
@@ -6,8 +6,8 @@ import android.support.annotation.Nullable;
 public interface VideoConversionProgressListener {
 	
 	/**
-	 * Progress percentage ranging from 0-1 where 0 == 0% and 1 == 100%
-	 * @param progressPercentage Progress percentage from 0.0 to 1.0
+	 * Callback to update on progress of the video conversion
+	 * @param progressPercentage Progress percentage from 0.0 to 1.0 where 0 == 0% and 1 == 100%
 	 * @param estimatedNumberOfMillisecondsLeft Estimated number of milliseconds left; note that
 	 *                                          this may be zero depending on the number of iterations
 	 *                                          that have been processed.
